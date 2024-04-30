a669ef202065f7a0fccf49f8ee383a26257d9fbb
The-greenpanter <judpenaca@unal.edu.co>
2024-04-29 19:41:25 -0500

    Clase 05 primeros scripts con SQL

---
+++ HenryClasesPTdata_10/Modulo2/Clase_05/Learning_SQL_Clase5.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Alumnos(
+	cedula_identindad INT NOT NULL Auto_increment,
+    nombre varchar (20),
+    apellido varchar (20),
+    fechaInicio date,
+    PRIMARY KEY (cedula_identindad)
+);

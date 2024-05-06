6d01a13aa2c3629b92c6c03d5f705d6eebfea630
The-greenpanter <judpenaca@unal.edu.co>
2024-05-05 21:53:48 -0500

    working with data

--- HenryClasesPTdata_10/Modulo2/Clase_05/Learning_SQL_Clase5.sql
+++ HenryClasesPTdata_10/Modulo2/Clase_05/Learning_SQL_Clase5.sql
@@ -1,13 +1,46 @@
+-- Clear cualquier record anterior
+DROP DATABASE IF EXISTS henry;
+
 -- INNODB
 CREATE DATABASE Henry;
+
 -- Accede a la DB
 USE Henry;
+
 -- Creo las tablas
 -- Primer componente de la BD
+CREATE TABLE Carrera (
+    idCarrera INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre_carrera VARCHAR(50)
+);
+
+CREATE TABLE Instructores (
+    ID_instructor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cedula_identidad INT NOT NULL UNIQUE,
+    nombre VARCHAR(20),
+    apellido VARCHAR(20),
+    fecha_nacimiento DATE,
+    fecha_incorporacion DATE
+);
+
+CREATE TABLE Cohorte (
+    ID_cohorte INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    codigo INT NOT NULL UNIQUE,
+    fecha_Inicio DATE,
+    fecha_Finalizacion DATE,
+    idCarrera INT NOT NULL, -- Cambio aquí
+    FOREIGN KEY (idCarrera) REFERENCES Carrera (idCarrera), -- Cambio aquí
+    FOREIGN KEY (ID_instructor) REFERENCES Instructores (ID_instructor)
+);
+
 CREATE TABLE Alumnos(
-	cedula_identindad INT NOT NULL Auto_increment,
-    nombre varchar (20),
-    apellido varchar (20),
-    fechaInicio date,
+    ID_alumnos INT NOT NULL AUTO_INCREMENT,
+    cedula_identindad INT NOT NULL UNIQUE,
+    nombre VARCHAR(20),
+    apellido VARCHAR(20),
+    fecha_nacimiento DATE,
+    fecha_ingreso DATE,
+    ID_cohorte INT NOT NULL,
+    FOREIGN KEY (ID_cohorte) REFERENCES Cohorte (ID_cohorte),
     PRIMARY KEY (cedula_identindad)
 );
